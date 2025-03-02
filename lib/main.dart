024c9b1979269b081a71bd6108735b7b3df1055c
きんとん <sararigame1@gmail.com>
2025-03-02 18:46:22 +0900

    UI改善: NavigationRailのラベルを修正し、ボタンのonPressedコールバックを明示化

--- lib/main.dart
+++ lib/main.dart
@@ -56,19 +56,19 @@ class MyHomePage extends StatelessWidget {
               extended: false,
               destinations: [
                 NavigationRailDestination(
-                  icon: Icon(Icons.home), 
-                  label: Text("Home")
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
                 ),
                 NavigationRailDestination(
-                  icon: Icon(Icons.favorite), 
-                  label: Text("Favorites")
-                )
-              ], 
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
               selectedIndex: 0,
               onDestinationSelected: (value) {
                 print('selected: $value');
               },
-            )
+            ),
           ),
           Expanded(
             child: Container(
@@ -97,29 +97,31 @@ class GeneratorPage extends StatelessWidget {
 
     return Center(
       child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: appState.toggleFavorite, 
-                  icon: Icon(icon),
-                  label: Text("Like"),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
-              ],
-            ),
-          ],
-        ),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
